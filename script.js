713acff124259caefd34f49b0c5ae541f122c1ec
Aditya Raj <akaushik2@gitaam.in>
2024-03-04 22:23:12 +0530

    goto

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const calendar = document.querySelector(".calendar"),
     date = document.querySelector(".date"),
     dayContainer = document.querySelector(".days"),
     prev = document.querySelector(".prev"),
-    next = document.querySelector(".next");
+    next = document.querySelector(".next"),
+    gotoBtn = document.querySelector(".goto-btn"),
+    todayBtn = document.querySelector(".today-btn"),
+    dataInput = document.querySelector(".data-input");
+
 
 let today = new Date();
 let activeDay;
@@ -77,3 +81,49 @@ function nextMonth(){
 
 prev.addEventListener("click", prevMonth);
 next.addEventListener("click", nextMonth);
+
+
+todayBtn.addEventListener("click", () => {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+  });
+  
+ 
+  dataInput.addEventListener("input", (e) => {
+    dataInput.value = dataInput.value.replace(/[^0-9/]/g, "");
+    if (dataInput.value.length === 2) {
+        dataInput.value += "/";
+    }
+    if (dataInput.value.length > 7) {
+        dataInput.value = dataInput.value.slice(0, 7);
+    }
+    if (e.inputType === "deleteContentBackward") {
+        if (dataInput.value.length === 3) {
+            dataInput.value = dataInput.value.slice(0, 2);
+        }
+    }
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+    console.log("here");
+    const dateArr = dataInput.value.split("/");
+    if (dateArr.length === 2) {
+        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    }
+    alert("Invalid Date");
+}
+
+  
+
+
+
+   
